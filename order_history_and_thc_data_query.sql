d72a17000484e1ac62b32224c07a09f628a49118
Oscary Antonio Dotel <95876398+DOTEL0Y@users.noreply.github.com>
2025-12-16 11:36:40 -0500

    Combine THC_DATA & ORDER_HISTORY
    ML dataset

---
+++ order_history_and_thc_data_query.sql
@@ -0,0 +1,22 @@
+SELECT 
+	thc_data.product_name,
+	thc_data.productid,
+	SUM(order_history.quantity) AS total_quantity_ordered,
+	SUM(order_history.total_price) as total_revenue,
+	thc_data.thca_percentage,
+	thc_data.total_cbd,
+	thc_data.cbga,
+	thc_data.total_cbg,
+	thc_data.delta_nine_thc
+	
+FROM 
+	thc_data
+INNER JOIN 
+	order_history ON TRUE
+CROSS JOIN LATERAL
+	UNNEST(order_history.productid) AS order_productid(productid)
+WHERE
+	thc_data.productid = order_productid.productid
+GROUP BY 
+	thc_data.product_name,
+	thc_data.productid;
